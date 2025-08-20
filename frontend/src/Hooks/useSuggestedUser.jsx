f6b1bc958ef49cf9a12f93be247e54824bd635fe
manish <manishzademz7@gmail.com>
2025-08-20 20:48:28 +0530

    fix error of suggested user

--- frontend/src/Hooks/useSuggestedUser.jsx
+++ frontend/src/Hooks/useSuggestedUser.jsx
@@ -1,18 +1,17 @@
 import { useEffect } from "react";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSuggestedUsers } from "../redux/slices/userSlice";
 import { useNavigate } from "react-router-dom";
 
-
-
 export default function useSuggestedUser() {
   const dispatch = useDispatch();
-const navigate = useNavigate();
+  const navigate = useNavigate();
+  const suggestedUsers = useSelector(state => state.user.suggestedUsers);
+
   useEffect(() => {
-    axios.get("http://localhost:8000/api/user/suggested", { withCredentials: true })
+    axios.get(`${import.meta.env.VITE_SERVER_URL}/api/user/suggested`, { withCredentials: true })
       .then(res => {
-        // Prevent storing HTML payloads
         if (typeof res.data === "string" && res.data.includes("<!DOCTYPE html>")) {
           console.error("Backend returned HTML instead of JSON:", res.data);
           return;
@@ -20,8 +19,10 @@ const navigate = useNavigate();
         dispatch(setSuggestedUsers(res.data));
       })
       .catch(err => {
-        console.error("Error fetching current user:", err);
-        navigate("/signin")
+        console.error("Error fetching suggested users:", err);
+        navigate("/signin");
       });
-  }, [dispatch]);
+  }, [dispatch, navigate]);
+
+  return suggestedUsers;
 }
