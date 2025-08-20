49cdaaf9756204bb898f9ef21890166f18bce1fc
manish <manishzademz7@gmail.com>
2025-08-20 20:58:53 +0530

    fix current user error

--- frontend/src/redux/slices/userSlice.js
+++ frontend/src/redux/slices/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    userData: null,
+    userData: [],
     suggestedUsers:null,
     profileData:null,
     following:[],
